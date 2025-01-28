8c2a71170ddc4c9e761116f30eb490f80c4c3a88
lindsaysperring <sperringlindsay@gmail.com>
2025-01-28 11:42:34 +1030

    Add project files.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace GetHymnLyricsv2.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace GetHymnLyricsv2.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
